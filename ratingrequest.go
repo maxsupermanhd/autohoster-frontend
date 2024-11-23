7b4d6497da5c748b973193e7de8725fa028616be
maxsupermanhd <q3.max.2011@ya.ru>
2024-11-23 19:16:14 +0300

    update rating request

--- ratingrequest.go
+++ ratingrequest.go
@@ -86,110 +86,81 @@ func ratingLookup(hash string, gameVersion string) Ra {
 		m.EloTextColorOverride = [3]int{0xff, 0x00, 0x00}
 		return m
 	}
-	var delo, dautoplayed, dautowon, dautolost, duuserid int
-	var duterm, dallowed, dadmin bool
-	var dname string
-	derr := dbpool.QueryRow(context.Background(), `select
-	identities.name, accounts.id, accounts.terminated, accounts.allow_host_request, accounts.superadmin, rating.elo, rating.played, rating.won, rating.lost
-from identities
-left join accounts on identities.account = accounts.id
-left join rating on accounts.id = rating.account
-left join rating_categories on rating.category = rating_categories.id
-where hash = $1 and category = 2`, hash).
-		Scan(&dname, &duuserid, &duterm, &dallowed, &dadmin, &delo, &dautoplayed, &dautowon, &dautolost)
-	if derr != nil {
-		if derr == pgx.ErrNoRows {
-			if m.Elo == "" {
-				wonCount := 0
-				err := dbpool.QueryRow(context.Background(), `select count(p)
-from players as p
-join identities as i on p.identity = i.id
-where p.usertype = 'winner' and i.hash = $1`, hash).Scan(&wonCount)
-				m.Details = "Casual noname"
-				m.NameTextColorOverride = [3]int{0x66, 0x66, 0x66}
-				m.EloTextColorOverride = [3]int{0xff, 0x44, 0x44}
-				if err != nil {
-					if err != pgx.ErrNoRows {
-						log.Println(err)
-					}
-					m.Elo = "Unknown player"
-				} else {
-					m.Elo = fmt.Sprintf("Unknown player (% 4d wins)", wonCount)
-				}
-			}
-		} else {
-			log.Print(derr)
+
+	var lid int
+	var lacc *int
+	var lname string
+	var lmod, lterm, ladmin bool
+
+	var lwinsZ *int
+	var lwins int
+
+	var lrating map[string]any
+
+	err := dbpool.QueryRow(context.Background(), `
+with
+	s1 as (select identities.id as lid,
+				identities.account as lacc,
+				coalesce(accounts.display_name, 'noname') as lname,
+				coalesce(accounts.allow_host_request, false) as lmod,
+				coalesce(accounts.terminated, false) as lterm,
+				coalesce(accounts.superadmin, false) as ladmin
+			from identities
+			left join accounts on accounts.id = identities.account
+			where hash = $1),
+	s2 as (select s1.lid as s2lid, coalesce(count(players), 0) as lwins
+			from players, s1
+			where players.usertype = 'winner' and players.identity = s1.lid
+			group by s1.lid),
+	s3 as (select json_build_object(
+				'account', rating.account,
+				'elo', rating.elo,
+				'played', rating.played,
+				'won', rating.won,
+				'lost', rating.lost,
+				't', 'elo') as r,
+				rating.account as racc
+			from rating, s1
+			where rating.account = s1.lacc and rating.category = 2)
+select lid, lacc, lname, lmod, lterm, ladmin, lwins, r
+from s1
+left join s2 on s1.lid = s2.s2lid
+left join s3 on s1.lacc = s3.racc`, hash).Scan(
+		&lid, &lacc, &lname, &lmod, &lterm, &ladmin,
+		&lwinsZ,
+		&lrating,
+	)
+
+	if lwinsZ != nil {
+		lwins = *lwinsZ
+	}
+
+	if err != nil {
+		if err != pgx.ErrNoRows {
+			m.Elo = "request failed"
+			log.Print(err)
+			return m
 		}
+		m.Details = "No information on that hash\n"
+		m.NameTextColorOverride = [3]int{0x66, 0x66, 0x66}
+		m.EloTextColorOverride = [3]int{0xff, 0x44, 0x44}
+		m.Elo = "Unknown player"
+		return m
+	}
+
+	if lacc == nil {
+		m.Details = "Not registered player\n"
+		m.NameTextColorOverride = [3]int{0x66, 0x66, 0x66}
+		m.EloTextColorOverride = [3]int{0xff, 0x44, 0x44}
+		m.Elo = fmt.Sprintf("Unknown player (% 4d wins)", lwins)
 		return m
 	}
 
 	if gameVersion != "" {
-		m.Name = dname
+		m.Name = lname
 	}
 
-	if duuserid > 0 {
-		m.Details += fmt.Sprintf("Rating: % 4d\n", delo)
-		m.Details = fmt.Sprintf("Played: % 4d\n", dautoplayed)
-		m.Details += fmt.Sprintf("Won: % 4d Lost: % 4d\n", dautowon, dautolost)
-		if dallowed {
-			m.Details += "Allowed to moderate and request rooms\n"
-		}
-	} else {
-		m.Details += "Not registered user.\n"
-	}
-	// if len(drenames) > 0 {
-	// 	m.Details += "Other names:"
-	// 	for _, v := range drenames {
-	// 		m.Details += "\n" + v
-	// 	}
-	// }
-	// m.Details += fmt.Sprintf("Elo: %d (#%d)\n", de, dep)
-
-	// if isAprilFools() {
-	// 	dbpool.QueryRow(context.Background(), `select elo2, autoplayed, autolost, autowon from players join accounts on accounts.wzprofile2 = players.id where autoplayed > 5 and accounts.id != 0 order by random() limit 1;`).Scan(&delo2, &dautoplayed, &dautolost, &dautowon)
-	// 	m.Level = rand.Intn(8)
-	// 	if duuserid == 14 || duuserid == 17 {
-	// 		m.Level = 8
-	// 	}
-	// }
-
-	if m.Elo == "" {
-		var pc string
-		if dautowon+dautolost > 0 {
-			pc = fmt.Sprintf("%03.1f%%", float64(100)*(float64(dautowon)/float64(dautowon+dautolost)))
-		} else {
-			pc = "-"
-		}
-		if duuserid != -1 && duuserid != 0 {
-			m.Elo = fmt.Sprintf("R[% 4d] % 4d %s", delo, dautoplayed, pc)
-		} else {
-			m.Elo = "Unauthorized player"
-			m.NameTextColorOverride = [3]int{0x66, 0x66, 0x66}
-			m.EloTextColorOverride = [3]int{0xff, 0x44, 0x44}
-		}
-	}
-
-	if dallowed {
-		m.Level = 7
-		if gameVersion != "" {
-			m.TagTextColorOverride = [3]int{0x11, 0xaa, 0x11}
-			m.Tag = "Moderator"
-		} else {
-			m.NameTextColorOverride = [3]int{0x11, 0xaa, 0x11}
-			m.Name = "Moderator"
-		}
-	}
-	if dadmin {
-		m.Level = 8
-		if gameVersion != "" {
-			m.Tag = "Admin"
-			m.TagTextColorOverride = [3]int{0x33, 0xff, 0x33}
-		} else {
-			m.NameTextColorOverride = [3]int{0x33, 0xff, 0x33}
-			m.Name = "Admin"
-		}
-	}
-	if duterm {
+	if lterm {
 		m.Level = 0
 		m.NameTextColorOverride = [3]int{0xff, 0x22, 0x22}
 		m.EloTextColorOverride = [3]int{0xff, 0x22, 0x22}
@@ -200,40 +171,92 @@ where p.usertype = 'winner' and i.hash = $1`, hash).Scan(&wonCount)
 			m.Name = ""
 		}
 		m.Elo = "Account terminated"
-	} else {
-		if dautoplayed < 5 || duuserid <= 0 {
+		return m
+	}
+
+	if lmod {
+		m.Level = 7
+		if gameVersion != "" {
+			m.TagTextColorOverride = [3]int{0x11, 0xaa, 0x11}
+			m.Tag = "Moderator"
+		} else {
+			m.NameTextColorOverride = [3]int{0x11, 0xaa, 0x11}
+			m.Name = "Moderator"
+		}
+	}
+	if ladmin {
+		m.Level = 8
+		if gameVersion != "" {
+			m.Tag = "Admin"
+			m.TagTextColorOverride = [3]int{0x33, 0xff, 0x33}
+		} else {
+			m.NameTextColorOverride = [3]int{0x33, 0xff, 0x33}
+			m.Name = "Admin"
+		}
+	}
+
+	ratingtype, ok := lrating["t"].(string)
+	if !ok {
+		m.Details = "Not participated in rated games\n"
+		m.NameTextColorOverride = [3]int{0x66, 0x66, 0x66}
+		m.EloTextColorOverride = [3]int{0x88, 0x44, 0x44}
+		m.Elo = fmt.Sprintf("Not rated (% 4d wins)", lwins)
+		return m
+	}
+	m.Details += fmt.Sprintf("Showing rating type %s\n", ratingtype)
+	switch ratingtype {
+	case "elo":
+		m.Details += "Showing rating category 3\n"
+
+		relo := int(lrating["elo"].(float64))
+		rplayed := int(lrating["played"].(float64))
+		rwon := int(lrating["won"].(float64))
+		rlost := int(lrating["lost"].(float64))
+
+		m.Details += fmt.Sprintf("Rating: % 4d\n", relo)
+		m.Details += fmt.Sprintf("Played: % 4d\n", rplayed)
+		m.Details += fmt.Sprintf("Won: % 4d Lost: % 4d\n", rwon, rlost)
+
+		pc := "-"
+		if rwon+rlost > 0 {
+			pc = fmt.Sprintf("%03.1f%%", float64(100)*(float64(rwon)/float64(rwon+rlost)))
+		}
+
+		m.Elo = fmt.Sprintf("R[% 4d] % 4d %s", relo, rplayed, pc)
+
+		if rplayed < 5 {
 			m.Dummy = true
 		} else {
 			m.Dummy = false
-			if dautolost == 0 {
-				dautolost = 1
+			if rlost == 0 {
+				rlost = 1
 			}
-			if dautowon >= 24 && float64(dautowon)/float64(dautolost) > 6.0 {
+			if rwon >= 24 && float64(rwon)/float64(rlost) > 6.0 {
 				m.Medal = 1
-			} else if dautowon >= 12 && float64(dautowon)/float64(dautolost) > 4.0 {
+			} else if rwon >= 12 && float64(rwon)/float64(rlost) > 4.0 {
 				m.Medal = 2
-			} else if dautowon >= 6 && float64(dautowon)/float64(dautolost) > 3.0 {
+			} else if rwon >= 6 && float64(rwon)/float64(rlost) > 3.0 {
 				m.Medal = 3
 			}
-			if delo > 1800 {
+			if relo > 1800 {
 				m.Star[0] = 1
-			} else if delo > 1550 {
+			} else if relo > 1550 {
 				m.Star[0] = 2
-			} else if delo > 1400 {
+			} else if relo > 1400 {
 				m.Star[0] = 3
 			}
-			if dautoplayed > 60 {
+			if rplayed > 60 {
 				m.Star[1] = 1
-			} else if dautoplayed > 30 {
+			} else if rplayed > 30 {
 				m.Star[1] = 2
-			} else if dautoplayed > 10 {
+			} else if rplayed > 10 {
 				m.Star[1] = 3
 			}
-			if dautowon > 60 {
+			if rwon > 60 {
 				m.Star[2] = 1
-			} else if dautowon > 30 {
+			} else if rwon > 30 {
 				m.Star[2] = 2
-			} else if dautowon > 10 {
+			} else if rwon > 10 {
 				m.Star[2] = 3
 			}
 		}
